202ae36045d0c4061d9a7c1eeabb17a3f61a0ac0
Julien PERIGNON <perignon.julien@gmail.com>
2015-09-09 23:05:28 +0200

    ajout de la gestion des players, + input et ajout de differentes libs js

--- js/main.js
+++
@@ -1,6 +0,0 @@
-/* global angular */
-var app = angular.module('musicBattleApp', []);
-
-app.run(function($rootScope) {
-  $rootScope.test = "Hi";
-});
---
+++ js/playerController.js
@@ -0,0 +1,33 @@
+/* global $ */
+
+/* PlayerController */
+angular.module('musicBattleApp').controller('PlayerController',function($rootScope,$scope){
+  $scope.players = [];
+  $scope.playerName = null;
+  $scope.playerSet = false;
+  
+  
+  //Hit when a player is ready
+  $scope.playerReady = function(){
+    console.log("Player " + $scope.playerName + " ready");
+     $scope.playerSet = $scope.addPlayer($scope.playerName);
+  };
+  
+  $scope.addPlayer = function(playerName){
+    if($.inArray(playerName,$scope.players) >=0){
+        $rootScope.displayError("Sorry, " + playerName + " already joined the party !")
+        return false;
+    }
+    $scope.players.push(playerName)
+    $rootScope.displayInformation(playerName + " joined the party !");
+    return true;
+  };
+  
+  var index = 0;
+  //Test only
+  $scope.simulateNewPlayer = function(){
+    if($rootScope.testMode)
+      $scope.addPlayer("Player "+index++);
+  }
+  
+});
