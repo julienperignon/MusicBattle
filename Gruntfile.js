02f7b3c735a4be4a7a3a0dc22e18d87f1cdb1140
alpaaps <alep.dev@gmail.com>
2015-10-29 01:03:08 +0100

    Ajout de bower pour separer les dependances servers et front

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,38 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     exec: {
-      start_socket_server : 'node socket.js',
-      start_web_server : 'node web.js',
+      start_socket_server: 'node socket.js',
+      start_web_server: 'node web.js',
     },
     concurrent: {
-        run_app: ['exec:start_socket_server', 'exec:start_web_server'],
+      run_app: ['exec:start_socket_server', 'exec:start_web_server'],
+    },
+    bowerInstall: {
+      target: { 
+        src: [
+          'index.html', 
+        ],
+ 
+        // Optional: 
+        // --------- 
+        cwd: '',
+        dependencies: true,
+        devDependencies: true,
+        exclude: [],
+        fileTypes: {},
+        ignorePath: '',
+        overrides: {}
+      }
     }
   });
-  
+
   grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-bower-install');
   
   // Default task(s).
-  grunt.registerTask('default', ['concurrent:run_app']);
+  grunt.registerTask('default', ['bowerInstall', 'concurrent:run_app']);
 };
--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Battle for the best music against your friends :)
 
 # Getting started
 After cloning the repository, execute the following commands : 
-* `$> npm install `  
+* `$> npm install `
+* `$> bower install `    
 * `$> grunt `
 
 Then open your browser on http://localhost:8080
